0598b6180c862d950720a1fff35082d672f8ac7f
Elias Norta <55353468+Elias227@users.noreply.github.com>
2025-11-20 16:26:52 +0200

    add hooks lab assignment 1

--- src/components/MediaRow.jsx
+++ src/components/MediaRow.jsx
@@ -14,7 +14,7 @@ const MediaRow = (props) => {
         <img src={item.thumbnail} alt={item.title} />
       </td>
       <td>{item.title}</td>
-      <td onClick={() => alert('Test')}>{item.description}</td>
+      <td>{item.description}</td>
       <td>{new Date(item.created_at).toLocaleString('fi-FI')}</td>
       <td>{item.filesize}</td>
       <td>{item.media_type}</td>
